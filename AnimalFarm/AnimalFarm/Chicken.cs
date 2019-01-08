ef8e8760bc812a9b3a6e74d58ef7dda86a6f37b3
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-08 09:11:04 +0100

    .

---
+++ AnimalFarm/AnimalFarm/Chicken.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    public class Chicken
+    {
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Chicken's name can't be empty!");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0 || value > 15)
+                {
+                    throw new Exception("Chicken's age must be between 0 and 15.");
+                }
+            }
+        }
+
+        public int EggsPerDay => CalculateProductionPerDay();
+
+        private int CalculateProductionPerDay()
+        {
+            return 1;
+        }
+    }
+}
---
+++ AnimalFarm/AnimalFarm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken1 = new Chicken("Erna", 12);
+
+            Console.WriteLine($"{chicken1.Name} is {chicken1.Age} years old and poops out {chicken1.EggsPerDay} eggs per day");
+        }
+    }
+}
